7c0b6e143f377325d2f67c44060d29345c9b9d4d
sajan <sajan.rai@asterdio.com>
2023-01-10 16:20:53 +0545

    changes 2

--- src/client/containers/DashboardPage.js
+++ src/client/containers/DashboardPage.js
@@ -23,7 +23,7 @@ const DashboardPage = () => {
         <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
           <InfoBox Icon={ShoppingCart}
                    color={pink600}
-                   title="Total Profit"
+                   title="Total Profit/income"
                    value="1500k"
           />
         </div>
